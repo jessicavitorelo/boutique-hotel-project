381248cb1e3917728c7abf6d6c994530bd4805e8
jessicavitorelo <90190136+jessicavitorelo@users.noreply.github.com>
2024-11-15 12:57:57 +0800

    footer!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,21 @@ module.exports = {
     './*.{html,js,md}'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Poppins', 'sans-serif'],
+      'serif': ['Merriweather', 'serif'],
+      'display': ['Oswald', 'serif'],
+    },
+    extend: {
+      colors: { 
+        cream: '#F4EFE6',
+        storm: '#686867',
+        green: '#5E6C5B',
+        white: '#FEFCF6',
+        sky: '#D6E0E2',
+
+      }
+    },
   },
   plugins: [],
 }
